5ebb7881d8bae2779bb298e90326be857d459f02
sarah438 <124069595+sarah438@users.noreply.github.com>
2024-12-16 22:24:24 -0500

    Update graph_c.py

--- graph_c.py
+++ graph_c.py
@@ -24,13 +24,13 @@ import pandas as pd
 # create a dataframe of the test number and its results
 # initialize list of lists
 #data = [[1, 194.000000], [2, 157.000000], [3, 189.000000], [4, 171.000000], [5, 168.000000], [6,180.000000], [7,190.000000],[8,167.000000],[9,178.000000], [10, 170.000000]]
-data = {'Test #': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 'Time (nanoseconds)': [194.000000, 157.000000, 189.000000, 171.000000, 168.000000, 180.000000, 190.000000, 167.000000, 178.000000, 170.000000]}
+data = {'Test #': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 'Time (seconds)': [.0000194, .000157, .000189, .000171, .000168, .000180, .000190, .000167, .000178, .000170]}
 
 # Create the pandas DataFrame
 df = pd.DataFrame(data)
 
 # Create a visualization
 #sns.relplot( data=data, x="total_bill", y="tip", col="time", ) hue="smoker",style="smoker", size="size",
-sns.scatterplot(x='Test #', y='Time (nanoseconds)', data=df)
+sns.scatterplot(x='Test #', y='Time (seconds)', data=df)
 # Display the plot
 plt.show()
