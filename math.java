8d16592917e12419899a25a43cc29667f1ccfa91
sarah438 <124069595+sarah438@users.noreply.github.com>
2024-12-16 21:30:32 -0500

    Add files via upload

---
+++ math.java
@@ -0,0 +1,18 @@
+class Calculate {
+    public static int add(int a, int b){
+        return (a+b);
+    }
+    public static int subtract(int a, int b){
+        return (a-b);
+    }
+    public static int multiply(int a, int b){
+        return (a*b);
+    }
+    public static int divide(int a, int b){
+        return (a/b);
+    }
+    public static void main(String[] args){
+        int ans = add(3,5);
+        System.out.println(ans);
+    }
+}
