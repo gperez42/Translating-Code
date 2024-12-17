7e4431232f7a20a9fa69c14d471d7e4b45730f49
sarah438 <124069595+sarah438@users.noreply.github.com>
2024-12-16 20:09:05 -0500

    Update transpiler1.c
    added a timer

--- transpiler1.c
+++ transpiler1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+// clock code from 5.2 powerpoint
+// double  get_clock() {
+//      struct timeval tv;
+//      int ok;
+//      ok = gettimeofday(&tv, (void *) 0);
+//      if (ok<0) {
+//              printf(“gettimeofday error”);
+//      }
+//      return (tv.tv_sec * 1.0 + tv.tv_usec * 1.0E-6);
+// }
 
 // Function to process a line of Java code and generate C code
 void processLine(char *line, FILE *outputFile) {
@@ -20,14 +32,15 @@ void processLine(char *line, FILE *outputFile) {
         fprintf(outputFile, "%s;\n", line);
     }
 
-    else if (strncmp(line, "import", 6) == 0 || strncmp(line, "", 0) == 0){}
+//    else if (strncmp(line, "import", 6) == 0 || strncmp(line, "", 0) == 0){}
     // Handle unsupported lines
     else {
         fprintf(stderr, "Unsupported line: %s\n", line);
     }
 }
-
 int main(int argc, char *argv[]) {
+//      float t0 = get_clock(); // start timer
+        clock_t t0 = clock(); // start timer
     if (argc != 3) {
         fprintf(stderr, "Usage: %s <input_file> <output_file>\n", argv[0]);
         return 1;
@@ -59,6 +72,9 @@ int main(int argc, char *argv[]) {
     fclose(inputFile);
     fclose(outputFile);
 
+        //float t1 = get_clock(); // end timer
+        clock_t t1 = clock();
+        printf("time per call: %f ns\n", (float)((t1-t0)) );
+
     return 0;
 }
-
