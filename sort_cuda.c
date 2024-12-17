fa9de4047f13178f7d38cbea206ee8f65bcf4f4f
Glenvelis <gperez42@smith.edu>
2024-12-16 22:37:34 -0500

    sort.java tested on both CPU and GPU

---
+++ sort_cuda.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+
+            int min_idx = i;
+
+            for (int j = i + 1; j < n; j++) {
+
+                    min_idx = j;
+
+            int temp = arr[i];
+
+            arr[i] = arr[min_idx];
+
+            arr[min_idx] = temp;           
+
+        int[] arr = { 64, 25, 12, 22, 11 };
+
+return 0;
+}
